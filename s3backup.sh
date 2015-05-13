2314a59b7d6e90b5eb16f6f9a2593e4dab712fe0
Jeeva Kailasam <jkailasam@netflix.com>
2015-05-12 22:04:54 -0700

    added s3cmd

--- s3backup.sh
+++ s3backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 RSYNC1="/usr/bin/boto-rsync"
 RSYNC2="/usr/bin/s3cmd"
-SOURCE="/sherlockpool/sherlock"
-DEST="s3://nflx-rsync-us-west-2/EnterprisePlatforms/sherlock_backup"
+SOURCE="/source/directory"
+DEST="s3://bucket_name/dest/directory"
 LOG=/root/s3backup.log
 
 echo -e "Backup to s3 started at `date` \n\n\n\n" >$LOG
 #$RSYNC1 -e $SOURCE $DEST >> $LOG 2>> $LOG
-$RSYNC2 --server-side-encryption --delete-removed -v --multipart-chunk-size-mb=15  ${SOURCE}/ ${DEST}/ >> $LOG 2>> $LOG
+$RSYNC2 sync -server-side-encryption --delete-removed -v --multipart-chunk-size-mb=15  ${SOURCE}/ ${DEST}/ >> $LOG 2>> $LOG
 echo -e "\n\n\n\nBackup completed at `date`" \n\n\n >>$LOG 2>&1
