1156d761401e13bb55e8c6effe1d3e39617b6040
Jeeva Kailasam <jkailasam@netflix.com>
2015-05-20 15:58:15 -0700

    added rsync.sh

---
+++ rsync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LOGSDIR=/root/logs
+
+cd /nfs/www
+for i in $(cat /root/kj); do
+nohup rsync -avh --exclude='no-sync' --exclude='.snapshot' --numeric-ids $i /nfsnew/www/ >${LOGSDIR}/${i}.out 2>${LOGSDIR}/${i}.error &
+# nohup rsync -ave “ssh -i keyfile”  --numeric-ids $i system1:/targetdir >${LOGSDIR}/${i}.out 2>${LOGSDIR}/${i}.erro
+
+        while true; do
+        sleep 30
+        COUNT=$(ps -ef | grep rsync | wc -l)
+        [[ $COUNT -le 20 ]] && break
+        done
+done
