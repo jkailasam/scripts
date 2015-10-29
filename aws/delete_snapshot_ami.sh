90bc187420311b99afd06a95065c9c4161ae84be
Jeeva Kailasam <kjeeva@gmail.com>
2015-10-29 15:18:34 -0700

    modified delete_snapshot_ami.sh

--- aws/delete_snapshot_ami.sh
+++ aws/delete_snapshot_ami.sh
@@ -1,25 +1,21 @@
 #!/bin/bash -x
 # this script  deletes the  AMI and corresponding disk images
 
-if [[ $# -ne 3 ]] ; then
-   echo "Usage: $(basename $0) [Image-id] [instanceid] [region]"
+if [[ $# -ne 2 ]] ; then
+   echo "Usage: $(basename $0) [Image-id] [region]"
    exit
 fi
 
 ImgId=$1
-InstanceId=$2
-Region=$3
+Region=$2
 TmpFile=/tmp/snapfile
-
-AZ=$3
-REGION="${AZ%?}"
-type=gp2
 
 aws ec2 describe-images --region $Region --image-ids $ImgId | jq -r '.Images[].BlockDeviceMappings[].Ebs.SnapshotId' > $TmpFile
 
 ## de-register Image
-aws ec2 deregister-image --image-id ImgId
+aws ec2 deregister-image --image-id $ImgId
 
+sleep 10
 for i in $(cat $TmpFile|egrep -v null); do
 	aws ec2 delete-snapshot --region $Region --snapshot-id $i
 done
