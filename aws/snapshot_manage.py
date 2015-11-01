a5f276a83724bb510dfd675194d5346cd7edea7a
Jeeva Kailasam <jkailasam@jk-mbp.local>
2015-10-31 22:10:46 -0700

    have a working snapshot manage script

--- aws/snapshot_manage.py
+++ aws/snapshot_manage.py
@@ -33,8 +33,6 @@ def get_resource_tags(resource_id):
                 resource_tags[tag.name] = tag.value
     return resource_tags
 
-
-
 def create_description(resource_id):
     desc = '%(policy)s_snapshot for %(vol_id)s taken on %(date)s' %{
     'policy' : policy, 'vol_id' : vol.id, 'date' : datetime.today().strftime('%d-%m-%Y at %H:%M:%S')
@@ -44,10 +42,8 @@ def create_description(resource_id):
 def create_delete_list(snap,policy):
     snapdesc = snap.description
     if (snapdesc.startswith(policy) and policy == policy):
-        deletelist.append(snap)
+        deletelist[snap] = snap.start_time
     return deletelist
-
-
 
 ## Define AWS EC2 and SNS Connections
 conn = boto.ec2.connect_to_region(region)
@@ -56,7 +52,7 @@ sns = boto.sns.connect_to_region(region)
 # vols = conn.get_all_volumes(filters={ 'tag:' + config['tag_name']: config['tag_value'] })
 vols = conn.get_all_volumes(filters={ 'tag:Snapshot': 'True' })
 
-
+## Create a new snapshot
 for vol in vols:
     description = create_description(vol)
     current_snap = vol.create_snapshot(description)
@@ -66,33 +62,28 @@ for vol in vols:
 
 
 if policy == 'daily':
-    snap_to_keep = 6
+    snap_to_keep = keep_daily
 elif policy == weekly:
-    snap_to_keep = 4
+    snap_to_keep = keep_weekly
 elif policy == monthly:
-    snap_to_keep = 2
+    snap_to_keep = Keep_monthly
 
-
-
-/// to delete the old snapshots
-
-01. Get the delete list
-def create_delete_list(snap,policy):
-    snapdesc = snap.description
-    if (snapdesc.startswith(policy) and policy == policy):
-        deletelist[snap] = snap.start_time
-    return deletelist
-
-for snap in snaps:
-    create_delete_list(snap,policy)
-
-02. Sort it by value
-    sorted_by_date = sorted(deletelist.values())
-
-03. Find out the oldest snap
-    for snap, start_time in deletelist.items():
-        if start_time == sorted_by_date[0]:
-            snap_to_delete = snap
-    return snap_to_delete
-
-///
+## delete the oldest snapshots
+for vol in vols:
+    snaps = vol.snapshots()
+    deletelist = {}
+    for snap in snaps:
+        deletelist = create_delete_list(snap,policy)
+        sorted_by_date = sorted(deletelist.values())
+    count = len(deletelist)
+    if count > snap_to_keep :
+        delta = count - snap_to_keep
+        for i in range(0,delta):
+            for snap, start_time in deletelist.items():
+                if start_time == sorted_by_date[i]:
+                    try:
+                        print "now deletng %(snap)s" %{'snap' : snap}
+                        response = snap.delete()
+                        print '%(snap)s deleted successully' %{'snap' : snap}
+                    except:
+                        print "Error deleing %(snap)s" %{'snap' : snap}
