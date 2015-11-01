b73baf700d754a4d6a1aedb3f4805a3cefe28575
Jeeva Kailasam <jkailasam@jk-mbp.local>
2015-10-31 23:22:19 -0700

    ok

--- aws/snapshot_manage.py
+++ aws/snapshot_manage.py
@@ -19,8 +19,15 @@ tag_value = 'True'
 keep_daily = 6
 keep_weekly = 4
 Keep_monthly = 2
-log_file = '/tmp/makesnapshots.log'
-policy = 'daily'
+policy = 'monthly'
+
+## Set number of snaps to keep
+if policy == 'daily':
+    snap_to_keep = keep_daily
+elif policy == 'weekly':
+    snap_to_keep = keep_weekly
+elif policy == 'monthly':
+    snap_to_keep = Keep_monthly
 
 ### Define Functions
 
@@ -39,11 +46,32 @@ def create_description(resource_id):
     }
     return desc
 
-def create_delete_list(snap,policy):
-    snapdesc = snap.description
-    if (snapdesc.startswith(policy) and policy == policy):
-        deletelist[snap] = snap.start_time
-    return deletelist
+def create_snapshot():
+    description = create_description(vol)
+    current_snap = vol.create_snapshot(description)
+    print '%(snap_id)s created for %(volume)s' %{
+    'snap_id' : current_snap, 'volume' : vol }
+
+def delete_snapshots():
+    snaps = vol.snapshots()
+    deletelist = {}
+    for snap in snaps:
+        snapdesc = snap.description
+        if (snapdesc.startswith(policy) and policy == policy):
+            deletelist[snap] = snap.start_time
+    sorted_by_date = sorted(deletelist.values())
+    count = len(deletelist)
+    if count > snap_to_keep :
+        delta = count - snap_to_keep
+        for i in range(0,delta):
+            for snap, start_time in deletelist.items():
+                if start_time == sorted_by_date[i]:
+                    try:
+                        print "now deleting %(policy)s %(snap)s" %{'policy': policy, 'snap' : snap}
+                        response = snap.delete()
+                        print '%(policy)s %(snap)s deleted successully' %{'policy': policy, 'snap' : snap}
+                    except:
+                        print "Error deleing %(snap)s" %{'snap' : snap}
 
 ## Define AWS EC2 and SNS Connections
 conn = boto.ec2.connect_to_region(region)
@@ -54,21 +82,20 @@ vols = conn.get_all_volumes(filters={ 'tag:Snapshot': 'True' })
 
 ## Create a new snapshot
 for vol in vols:
-    description = create_description(vol)
-    current_snap = vol.create_snapshot(description)
-    print '%(snap_id)s created for %(volume)s' %{
-    'snap_id' : current_snap, 'volume' : vol
-    }
+    create_snapshot()
+    delete_snapshots()
 
 
-if policy == 'daily':
-    snap_to_keep = keep_daily
-elif policy == weekly:
-    snap_to_keep = keep_weekly
-elif policy == monthly:
-    snap_to_keep = Keep_monthly
 
-## delete the oldest snapshots
+'''
+
+def create_delete_list(snap,policy):
+    snapdesc = snap.description
+    if (snapdesc.startswith(policy) and policy == policy):
+        deletelist[snap] = snap.start_time
+    return deletelist
+
+
 for vol in vols:
     snaps = vol.snapshots()
     deletelist = {}
@@ -82,8 +109,9 @@ for vol in vols:
             for snap, start_time in deletelist.items():
                 if start_time == sorted_by_date[i]:
                     try:
-                        print "now deletng %(snap)s" %{'snap' : snap}
+                        print "now deleting %(snap)s" %{'snap' : snap}
                         response = snap.delete()
                         print '%(snap)s deleted successully' %{'snap' : snap}
                     except:
                         print "Error deleing %(snap)s" %{'snap' : snap}
+'''
