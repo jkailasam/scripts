4cd4b89a4c7e75fc41ace8e892a029c43d9fcdb4
Jeeva Kailasam <kjeeva@gmail.com>
2016-01-13 12:34:00 -0800

    iam_key colors added

--- aws/iam_key.py
+++ aws/iam_key.py
@@ -4,23 +4,31 @@ iam = boto3.resource('iam')
 iamc = boto3.client('iam')
 
 string = 'LastUsedDate'
-
 users = iam.users.all()
+
 # Metadata = iamc.list_access_keys(UserName=user.user_name)
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARYELLO = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 for user in users:
     for key in user.access_keys.all():
         username = user.user_name
-        fusername = username.ljust(35)
         accessid = key.id
         status = key.status
         lastused = iamc.get_access_key_last_used(AccessKeyId=accessid)
         if status == "Active":
             if string in lastused['AccessKeyLastUsed']:
                 date = lastused['AccessKeyLastUsed'][string].strftime('%Y-%m-%d %H:%M UTC')
-                print "User: {0}  Key: {1}     Last_Used: {2}".format(fusername,accessid,date)
+                print("User: {:<35}  Key: {}     Last_Used: {}".format(username,accessid,date))
             else:
-                print "User: {0}  Key: {1}     Key_Active_but_Never_Used".format(fusername,accessid)
+                print(bcolors.FAIL + "User: {:<35}  Key: {}     Key_Active_but_Never_Used".format(username,accessid) + bcolors.ENDC)
 
 
 
