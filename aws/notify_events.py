20801f424c9c8d1f4514ca7e3339766cc63f0d77
Jeeva Kailasam <kjeeva@gmail.com>
2016-01-19 17:40:18 -0800

    event_notify tags added

--- aws/notify_events.py
+++ aws/notify_events.py
@@ -20,6 +20,25 @@ filters = [{'Values': ['instance-stop', 'instance-reboot', 'system-reboot', 'sys
 def addtoddb(Instance_Id,Event_Code,Event_Description,Event_Time):
     table.put_item(Item={'InstanceId':Instance_Id,'Region':region,'EventCode':Event_Code, 'EventDescription':Event_Description,'EventTime':Event_Time})
 
+def process_tags(ec2,Instance_Id):
+    tags = ec2.Instance(Instance_Id).tags
+    email = ''
+    app = ''
+    Owner = ''
+    for tag in tags:
+        if tag['Key']  == 'Name':
+            app = tag['Value']
+        elif tag['Key'] == 'eboxapp':
+            app = tag['Value']
+        if tag['Key']  == 'eboxemail':
+            email = tag['Value']
+        if tag['Key'] == 'eboxuser':
+            Owner = tag['Value']
+        elif tag['Key'] == 'Owner':
+            Owner = tag['Value']
+    return {'app':app, 'email':email, 'Owner':Owner}
+
+
 def check_events(region):
     #for region in regions:
     ec2 = boto3.resource('ec2',region)
@@ -27,17 +46,18 @@ def check_events(region):
     InstanceStatuses = ec2client.describe_instance_status(Filters=filters)['InstanceStatuses']
 
     for InstanceStatus in InstanceStatuses:
-        Instance_id = InstanceStatus['InstanceId']
+        Instance_Id = InstanceStatus['InstanceId']
+        TAGS = process_tags(ec2,Instance_Id)
         Events = InstanceStatus['Events']
         for Event in Events:
             Event_Code = Event['Code']
             Event_Description = Event['Description']
             Event_Time = Event['NotBefore'].strftime('%Y-%m-%d %H:%M UTC')
             #Event_Time = Event['NotBefore']
-            print ("Instance {0} is Scheduled to {1}".format(Instance_id,Event_Code))
+            print ("Instance {0} is Scheduled to {1}".format(Instance_Id,Event_Code))
             print ("Reason: {0}".format(Event_Description))
             print ("Scheduled Time: {0}".format(Event_Time))
-            addtoddb(Instance_id,Event_Code,Event_Description,Event_Time)
+            addtoddb(Instance_Id,Event_Code,Event_Description,Event_Time)
 
 if __name__ == '__main__':
    for region in regions:
