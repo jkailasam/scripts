985cf48ab74dd7ffc74adc81ce29e91277027d9e
Jeeva Kailasam <kjeeva@gmail.com>
2016-01-20 18:06:51 -0800

    notify_events - dev and legacy accounts added

--- aws/notify_events.py
+++ aws/notify_events.py
@@ -2,6 +2,7 @@ __author__ = 'Jeeva Kailasam'
 import datetime
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+from boto3.session import Session
 import sys
 
 regions = ['us-west-1', 'us-west-2', 'us-east-1']
@@ -11,15 +12,18 @@ dynamodb=boto3.resource('dynamodb','us-west-2')
 ddbclient = boto3.client('dynamodb','us-west-2')
 table = dynamodb.Table(table_name)
 
-
+legacy = Session(profile_name='legacy')
+dev = Session(profile_name='dev')
+prod = Session(profile_name='prod')
+account = 'prod'
 
 filters = [{'Values': ['instance-stop', 'instance-reboot', 'system-reboot', 'system-maintenance', 'instance-retirement'], 'Name': 'event.code'}]
 
 
 
-def addtoddb(Instance_Id,Event_Code,Event_Description,Event_Time,app,Owner,email):
+def addtoddb(Instance_Id,account,Event_Code,Event_Description,Event_Time,app,Owner,email):
     table.put_item(Item={'InstanceId':Instance_Id,'Region':region,'EventCode':Event_Code, 'EventDescription':Event_Description,\
-                         'EventTime':Event_Time,'App':app,'Owner':Owner,'email':email})
+                         'EventTime':Event_Time,'App':app,'Owner':Owner,'email':email,'Account':account})
 
 def process_tags(ec2,Instance_Id):
     tags = ec2.Instance(Instance_Id).tags
@@ -40,10 +44,11 @@ def process_tags(ec2,Instance_Id):
     return {'app':app, 'email':email, 'Owner':Owner}
 
 
-def check_events(region):
-    #for region in regions:
-    ec2 = boto3.resource('ec2',region)
-    ec2client=boto3.client('ec2',region)
+def check_events(aws,region):
+    #ec2 = boto3.resource('ec2',region)
+    #ec2client=boto3.client('ec2',region)
+    ec2 = aws.resource('ec2',region)
+    ec2client = aws.client('ec2',region)
     InstanceStatuses = ec2client.describe_instance_status(Filters=filters)['InstanceStatuses']
 
     for InstanceStatus in InstanceStatuses:
@@ -58,11 +63,17 @@ def check_events(region):
             print ("Instance {0} is Scheduled to {1}".format(Instance_Id,Event_Code))
             print ("Reason: {0}".format(Event_Description))
             print ("Scheduled Time: {0}".format(Event_Time))
-            addtoddb(Instance_Id,Event_Code,Event_Description,Event_Time,TAGS['app'],TAGS['Owner'],TAGS['email'])
+            addtoddb(Instance_Id,account,Event_Code,Event_Description,Event_Time,TAGS['app'],TAGS['Owner'],TAGS['email'])
 
 if __name__ == '__main__':
-   for region in regions:
-       check_events(region)
+    for account in 'prod','dev','legacy':
+        aws = Session(profile_name=account)
+        for region in regions:
+            check_events(aws,region)
+
+
+
+
 
 
 
