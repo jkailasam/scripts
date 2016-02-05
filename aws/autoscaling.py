396e54a30f4c342485ff93a8414a5482e4ff514c
Jeeva Kailasam <kjeeva@gmail.com>
2016-02-04 16:23:03 -0800

    autoscalling.py added

---
+++ aws/autoscaling.py
@@ -0,0 +1,15 @@
+import boto3
+
+autoscaling = boto3.client('autoscaling', 'us-west-2')
+ASGS=autoscaling.describe_auto_scaling_groups()['AutoScalingGroups']
+
+for ASG in ASGS:
+    asgname = ASG['AutoScalingGroupName']
+    if ('visine-imap' in asgname)  or ('visine-smtp' in asgname) :
+        Instances = ASG['Instances']
+        for Instance in Instances:
+            instanceid Instances
+
+
+
+
