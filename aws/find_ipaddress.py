4e4cc7779181f4e9bef19265dd3338269b33fab6
Jeeva Kailasam <kjeeva@gmail.com>
2016-04-20 10:49:58 -0700

    added jsonify to find_ipaddress.py

--- aws/find_ipaddress.py
+++ aws/find_ipaddress.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, Response
 from netaddr import IPNetwork, IPAddress
 import boto3
 import json
@@ -14,7 +14,7 @@ def account_region(file,addr):
     with open (file,'r') as vpcs:
         for line in vpcs:
             line = line.strip().split(',')
-            subnet = line[3]
+            #subnet = line[3]
             if IPAddress(addr) in IPNetwork(line[3]):
                 return [line[0], line[1], line[2]]
                 break
@@ -23,8 +23,10 @@ def account_region(file,addr):
 def lip_details(ec2c,region,addr):
     filter = [{'Name':'addresses.private-ip-address', 'Values':[addr]}]
     lip = ec2c.describe_network_interfaces(Filters = filter)['NetworkInterfaces']
-    json.JSONEncoder.default = lambda self, obj: (obj.isoformat() if isinstance(obj, datetime.datetime) else None)
-    return json.dumps(lip)
+    ## fix json error for datetime object
+    #json.JSONEncoder.default = lambda self, obj: (obj.isoformat() if isinstance(obj, datetime.datetime) else None)
+    #return json.dumps(lip)
+    return jsonify(lip[0])
 
 
 def eip_details(addr):
@@ -42,6 +44,22 @@ def eip_details(addr):
         #return json.dumps('[]')
         #return json.dumps(eip)
 
+
+def eip_details2(addr):
+    filter = [{'Name': 'addresses.association.public-ip', 'Values': [addr]}]
+    for region in regions:
+        ec2c = boto3.client('ec2', region)
+        eip = ec2c.describe_network_interfaces(Filters = filter)['NetworkInterfaces']
+        ## fix json error for datetime object
+        #json.JSONEncoder.default = lambda self, obj: (
+        #obj.isoformat() if isinstance(obj, datetime.datetime) else None)
+        if len(eip)>= 1:
+            #return json.dumps(eip)
+            return jsonify(eip[0])
+            break
+    if not eip:
+        return '[]'
+
 def find_info(addr):
     if addr.startswith('10.200.') or addr.startswith('10.201.') or addr.startswith('100.127.'):
         account_info = account_region(file,addr)
@@ -50,17 +68,18 @@ def find_info(addr):
         ec2c = boto3.client('ec2',region)
         return lip_details(ec2c, region, addr)
     else:
-        eip = eip_details(addr)
+        eip = eip_details2(addr)
         return eip
-
 
 @app.route('/ip/<addr>')
 def find_details(addr):
     output = find_info(addr)
-    if len(output) > 2:
-        return output
-    else:
-        return 'IP Address not found'
+    #if len(output) > 2:
+    return output
+    #else:
+        #return 'IP Address not found'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
