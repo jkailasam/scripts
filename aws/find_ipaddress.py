b2f9992cb82751b6b4f4cfe56e64ee1d1f865df1
Jeeva Kailasam <kjeeva@gmail.com>
2016-04-21 15:41:18 -0700

    sessin info modified with assume role to find_ipaddress

--- aws/find_ipaddress.py
+++ aws/find_ipaddress.py
@@ -1,85 +1,83 @@
 from flask import Flask, jsonify, Response
 from netaddr import IPNetwork, IPAddress
 import boto3
-import json
-import datetime
+from boto3.session import Session
 
 app = Flask(__name__)
+accounts = ['prod','dev','legacy']
 regions = ['us-west-2','us-west-1','us-east-1']
-#addr = '10.201.96.50'
-#addr = '54.191.126.200'
 file = '/tmp/vpc'
+
 
 def account_region(file,addr):
     with open (file,'r') as vpcs:
         for line in vpcs:
             line = line.strip().split(',')
-            #subnet = line[3]
             if IPAddress(addr) in IPNetwork(line[3]):
                 return [line[0], line[1], line[2]]
                 break
 
+def get_creds(account):
+    sts = boto3.client('sts')
+    if account == 'dev':
+        creds = sts.assume_role(RoleArn='arn:aws:iam::020769165682:role/whatsthisip',
+        RoleSessionName="AssumeRoleSession1")['Credentials']
+    elif account == 'legacy':
+        creds = sts.assume_role(RoleArn='arn:aws:iam::020661041473:role/whatsthisip',
+        RoleSessionName="AssumeRoleSession1")['Credentials']
+    if account == 'prod':
+        aws = Session()
+    elif account == 'dev' or account == 'legacy':
+        aws = Session(aws_access_key_id = creds['AccessKeyId'],
+        aws_secret_access_key = creds['SecretAccessKey'],
+        aws_session_token = creds['SessionToken'])
+    else:
+        print('Invalid Account')
+    return aws
 
-def lip_details(ec2c,region,addr):
+
+def lip_details(region, account, addr):
     filter = [{'Name':'addresses.private-ip-address', 'Values':[addr]}]
+    ec2c = Session(profile_name=account).client('ec2', region)
     lip = ec2c.describe_network_interfaces(Filters = filter)['NetworkInterfaces']
-    ## fix json error for datetime object
-    #json.JSONEncoder.default = lambda self, obj: (obj.isoformat() if isinstance(obj, datetime.datetime) else None)
-    #return json.dumps(lip)
-    return jsonify(lip[0])
-
+    if not lip:
+        lip = {'Result': 'Local IP address not found'}
+    elif len(lip[0].keys()) >= 1:
+        lip = lip[0]
+        lip['Account_Info'] = [account,region]
+    return lip
 
 def eip_details(addr):
-    filter = [{'Name':'public-ip', 'Values':[addr]}]
-    for region in regions:
-        ec2c = boto3.client('ec2', region)
-        eip = ec2c.describe_addresses(Filters = filter)['Addresses']
-        #print('eip is: {}'.format(eip))
-        if len(eip)>=1:
-            eip.append({'region':region})
-            return json.dumps(eip)
-            break
-    if not eip:
-        return '[]'
-        #return json.dumps('[]')
-        #return json.dumps(eip)
-
-
-def eip_details2(addr):
     filter = [{'Name': 'addresses.association.public-ip', 'Values': [addr]}]
-    for region in regions:
-        ec2c = boto3.client('ec2', region)
-        eip = ec2c.describe_network_interfaces(Filters = filter)['NetworkInterfaces']
-        ## fix json error for datetime object
-        #json.JSONEncoder.default = lambda self, obj: (
-        #obj.isoformat() if isinstance(obj, datetime.datetime) else None)
-        if len(eip)>= 1:
-            #return json.dumps(eip)
-            return jsonify(eip[0])
+    for account in accounts:
+        for region in regions:
+            ec2c = Session(profile_name=account).client('ec2', region)
+            eip = ec2c.describe_network_interfaces(Filters = filter)['NetworkInterfaces']
+            if not eip:
+                eip = {'Result': 'EIP address Not found'}
+            elif len(eip[0].keys())>= 1:
+                eip = eip[0]
+                eip['Account_Info'] = [account,region]
+                break
+        if len(eip.keys()) >1:
             break
-    if not eip:
-        return '[]'
+    return eip
 
 def find_info(addr):
     if addr.startswith('10.200.') or addr.startswith('10.201.') or addr.startswith('100.127.'):
         account_info = account_region(file,addr)
-        profile = account_info[0]
         region = account_info[1]
-        ec2c = boto3.client('ec2',region)
-        return lip_details(ec2c, region, addr)
+        account = account_info[0]
+        return lip_details(region, account, addr)
     else:
-        eip = eip_details2(addr)
+        eip = eip_details(addr)
         return eip
 
 @app.route('/ip/<addr>')
 def find_details(addr):
     output = find_info(addr)
-    #if len(output) > 2:
-    return output
-    #else:
-        #return 'IP Address not found'
+    return jsonify(output)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
