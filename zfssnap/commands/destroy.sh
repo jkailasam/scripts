3d3e32428fe03e701e281bfd57fd8446fb78a9b6
Jeeva Kailasam <kjeeva@gmail.com>
2016-05-16 13:41:36 -0700

    few more changes

--- zfssnap/commands/destroy.sh
+++ zfssnap/commands/destroy.sh
@@ -18,9 +18,6 @@ ${0##*/} destroy [ options ] zpool/filesystem ...
 OPTIONS:
   -D           = Delete *all* zfsnap snapshots — regardless of their TTL
                  expiration — on all ZFS file systems that follow this option
-  -F age       = Force delete all snapshots exceeding "age" — regardless
-                 of their TTL expiration — on all ZFS file systems that
-                 follow this option
   -h           = Print this help and exit
   -n           = Dry-run. Perform a trial run with no actions actually performed
   -r           = Operate recursively on all ZFS file systems after this option
@@ -32,6 +29,52 @@ EOF
     exit 20
 }
 
+Main_function(){
+    Info "Now processing $SNAPSHOT"
+    FileSystem=${SNAPSHOT%%@*}
+    SnapShotName=${SNAPSHOT##*@}
+    TTL=${SnapShotName##*--}
+    TTL_TYPE=$(echo -n $TTL | tail -c 1)
+    TTL_VALUE=$(echo -n $TTL | sed -e '$s/\(.\{1\}\)$//' -e 's/^0*//')
+
+    case $TTL_TYPE in
+        m) SecToKeep=$(expr $TTL_VALUE \* 60);;
+        h) SecToKeep=$(expr $TTL_VALUE \* 3600);;
+        d) SecToKeep=$(expr $TTL_VALUE \* 86400);;
+        w) SecToKeep=$(expr $TTL_VALUE \* 86400 \* 7);;
+        M) SecToKeep=$(expr $TTL_VALUE \* 86400 \* 30);;
+        y) SecToKeep=$(expr $TTL_VALUE \* 86400 \* 365);;
+        *) Warn "TTL is not valid; Skipping to next snapshot"
+	   echo -e "\n"
+           continue;;
+    esac
+
+    # convert the snapshot time in epoch seconds
+    pre_date="${SnapShotName%$DATE_PATTERN*}"
+    post_date="${SnapShotName##*$DATE_PATTERN}"
+    snapshot_date="${SnapShotName##$pre_date}"
+    snapshot_date=${snapshot_date%%$post_date}
+    # Confirm date in the snapshot name is in the right format
+    if ! [ -z "${snapshot_date##$DATE_PATTERN}" ] ; then
+        Warn "Date is not in the Valid format. Skipping to the next snapshot"
+	echo -e "\n"
+        continue
+    fi
+
+    # Find out if the snapshot TTL has expired, delete it if expired
+    date_formated=$(echo $snapshot_date|sed -e s'/_/ /' -e s'/\./:/g')
+    snapshot_creaed_sec=$(date -d "$date_formated" +%s)
+    snap_expire_sec=$(expr $snapshot_creaed_sec + $SecToKeep)
+    cur_date_in_sec=$(date +%s)
+    if [[ $cur_date_in_sec -gt $snap_expire_sec ]] ; then
+        Info "Snapshot Expired.. Deleting it now"
+        RM_SNAPSHOTS $SNAPSHOT
+    else
+        Info "Snapshot not expired yet... Skipping"
+    fi
+    echo -e "\n"
+}
+
 if [[ $# -lt 1 ]] ; then
     Help
 fi
@@ -41,11 +84,6 @@ OPTIND=1
 while getopts :DF:hnp:PrRz OPT; do
     case "$OPT" in
         D) DELETE_ALL_SNAPSHOTS='true';;
-        F) ValidTTL "$OPTARG" || Fatal "Invalid TTL: $OPTARG"
-           [ "$OPTARG" = 'forever' ] && Fatal '-F does not accept the "forever" TTL'
-           FORCE_AGE_TTL=$OPTARG
-           FORCE_DELETE_BY_AGE='true'
-           ;;
         h) Help;;
         n) DRY_RUN='true';;
         r) RECURSIVE='true';;
@@ -62,46 +100,15 @@ if [ -n "$1" ]; then
     FileSystem=$1
     ZFS_SNAPSHOTS=$($ZFS list -H -o name -s name -t snapshot -r $1) >&2 || Fatal "'$1' does not exist!"
     for SNAPSHOT in $ZFS_SNAPSHOTS; do
-        Info "Now processing $SNAPSHOT"
-        FileSystem=${SNAPSHOT%%@*}
-        SnapShotName=${SNAPSHOT##*@}
-        TTL=${SnapShotName##*--}
-        TTL_TYPE=$(echo -n $TTL | tail -c 1)
-        TTL_VALUE=$(echo -n $TTL | sed -e '$s/\(.\{1\}\)$//' -e 's/^0*//')
-
-        case $TTL_TYPE in
-            m) SecToKeep=$(expr $TTL_VALUE \* 60);;
-            h) SecToKeep=$(expr $TTL_VALUE \* 3600);;
-            d) SecToKeep=$(expr $TTL_VALUE \* 86400);;
-            w) SecToKeep=$(expr $TTL_VALUE \* 86400 \* 7);;
-            M) SecToKeep=$(expr $TTL_VALUE \* 86400 \* 30);;
-            y) SecToKeep=$(expr $TTL_VALUE \* 86400 \* 365);;
-            *) Warn "TTL is not valid; Skipping to next snapshot"
-               continue;;
-        esac
-
-        # convert the snapshot time in epoch seconds
-        pre_date="${SnapShotName%$DATE_PATTERN*}"
-        post_date="${SnapShotName##*$DATE_PATTERN}"
-        snapshot_date="${SnapShotName##$pre_date}"
-        snapshot_date=${snapshot_date%%$post_date}
-        # Confirm date in the snapshot name is in the right format
-        if ! [ -z "${snapshot_date##$DATE_PATTERN}" ] ; then
-            Warn "Date is not in the Valid format. Skipping to the next snapshot"
-            continue
-        fi
-
-        # Find out if the snapshot TTL has expired, delete it if expired
-        date_formated=$(echo $snapshot_date|sed -e s'/_/ /' -e s'/\./:/g')
-        snapshot_creaed_sec=$(date -d "$date_formated" +%s)
-        snap_expire_sec=$(expr $snapshot_creaed_sec + $SecToKeep)
-        cur_date_in_sec=$(date +%s)
-        if [[ $cur_date_in_sec -gt $snap_expire_sec ]] ; then
-            Info "Snapshot Expired.. Deleting it now"
+        ## delete all snaphot if -D flag is set
+        if [[ $DELETE_ALL_SNAPSHOTS = true ]]; then
+	    Info "Now processing $SNAPSHOT"
             RM_SNAPSHOTS $SNAPSHOT
+            echo -e "\n"
+        elif [[ $RECURSIVE = false ]] ; then
+            [[ $(echo $SNAPSHOT | grep ^${FileSystem}@) ]] && Main_function
         else
-            Info "Snapshot not expired yet... Skipping"
+            Main_function
         fi
-        echo -e "\n"
     done
 fi
